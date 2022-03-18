ba951357ec20568dd4b0ccc826779cc797e455ef
sashanka1 <sashankasekharsamal777@gmail.com>
2022-03-19 01:01:37 +0530

    done
    done

--- 9march-mongoRelationship/index.js
+++ 9march-mongoRelationship/index.js
@@ -27,10 +27,18 @@ const bookschema=mongoose.Schema(
         body:{type:String,require:true},
         outhorId:{type:mongoose.Schema.Types.ObjectId,ref:"author"},
         outhor2Id:{type:mongoose.Schema.Types.ObjectId,ref:"author"},
-        sectionId:{type:mongoose.Schema.Types.ObjectId,ref:"Section"}
+        sectionId:{type:mongoose.Schema.Types.ObjectId,ref:"Section"},
+        checkedin_Time:{type:String,default:null},
+        checkedout_Time:{type:String,default:null},
     }
 );
+//6234d91a514d5eb7f12df392
+//6234d9577bebcbdb90eb5849  author id
 
+//bookid
+//6234db76b5b17699e2b0f1d3
+//6234dbfc14ed1bdfba2ed95d
+//6234dc84bb002ed70e493cc7
 const Books=mongoose.model("book",bookschema);
 
 const authorScima=mongoose.Schema(
@@ -44,8 +52,36 @@ const Author=mongoose.model("author",authorScima);
 
 
 
-app.get("/books")
+app.post("/section",async(req,res)=>{
+    try {
+        const section=await Section.create(req.body)
+        return res.status(200).send(section);
+        
+    } catch (error) {
+        return res.status(500).send({message:error.message})
+    }
+});
 
+app.post("/author",async(req,res)=>{
+    try {
+        const author=await Author.create(req.body)
+    return res.status(200).send(author); 
+    } catch (error) {
+        return res.status(500).send({message:error.message})
+    }
+   
+})
+
+
+app.post("/book",async(req,res)=>{
+    try {
+        const book=await Books.create(req.body)
+    return res.status(200).send(book); 
+    } catch (error) {
+        return res.status(500).send({message:error.message})
+    }
+   
+})
 
 app.listen(6001,async()=>{
     try {
